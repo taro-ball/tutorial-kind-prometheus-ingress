6d9c8d3c2eab33fcd839ea7e2f72b5179a406cc1
Your Name <you@example.com>
2025-11-22 14:43:07 -0500

    fix prometheus-community helm repo

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 kind create cluster --config 3node.yaml
 
-helm install prometheus prometheus-community/prometheus --create-namespace --namespace prom
+# install prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts && helm repo update
+helm install prometheus prometheus-community/prometheus --create-namespace --namespace prom || exit 1
